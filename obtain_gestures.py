e2e18178d0325931edc6bf41ba94fab95d578e0c
Jordan Zdimirovic <jordstar20001@gmail.com>
2021-11-10 16:35:02 +1100

    Further implementation.
    Things to iron out:
    - How do we get the label from the handedness classification?

--- obtain_gestures.py
+++ obtain_gestures.py
@@ -15,6 +15,16 @@ HANDS_DET_CONF = 0.5
 HANDS_MOD_COMPLEX = 0
 HANDS_TRACK_CONF = 0.5
 def main():
+    folder = input("What are we classifying? : ")
+    save_index = 0
+    save_location = "data/gestures/" + folder + "/data.csv"
+
+    def save_data(hands_res):
+        if hands_res.multi_hand_landmarks and hands_res.multi_handedness:
+            for hand_index in hands_res.multi_hand_landmarks:
+                handedness = hands_res.multi_handedness[hand_index]
+                for datapoint in hands_res.multi_hand_landmarks:
+
     # Establish webcam connection
     cam = cv2.VideoCapture(0)
     # Create mediapipe hands classifier
@@ -34,16 +44,23 @@ def main():
         
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         hands_res = hands_mdl.process(frame)
-        if hands_res.multi_hand_landmarks:
-            for hand in hands_res.multi_hand_landmarks:
+        if hands_res.multi_hand_landmarks and hands_res.multi_handedness:
+            for hand_index in hands_res.multi_hand_landmarks:
+                landmarks = hands_res.multi_hand_landmarks[hand_index]
+                handedness = hands_res.multi_handedness[hand_index]
+                print(handedness)
+                #print(hand)
                 mp_drawing.draw_landmarks(
                     frame,
-                    hand,
+                    landmarks,
                     mp_hands.HAND_CONNECTIONS,
                     mp_drawing_styles.get_default_hand_landmarks_style(),
                     mp_drawing_styles.get_default_hand_connections_style()
                 )
         cv2.imshow("Hands!", frame)
+
+        if cv2.waitKey(1) == 13:
+            save_data(hands_res)
 
         if cv2.waitKey(1) == 27:
             break
